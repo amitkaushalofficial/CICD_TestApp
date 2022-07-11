5eba0c0257028e93d7368e08ea03c64cbef1d217
AmitKaushal <amit.kaushal@appinventiv.com>
2022-07-12 00:58:46 +0530

    android yaml update

--- app/src/main/java/com/example/cicd_testapp/MainActivity.kt
+++ app/src/main/java/com/example/cicd_testapp/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         Toast.makeText(applicationContext, "Test", Toast.LENGTH_LONG).show()
     }
 }
